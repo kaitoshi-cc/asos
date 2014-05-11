747c8159f1f7c336d3056342355c5a5f27bb6f4c
Kai <tkai8@users.noreply.github.com>
2014-05-11 11:28:34 +0900

    added asos message header file.

--- src/servers/asos_common/ASOS_Node.cpp
+++ src/servers/asos_common/ASOS_Node.cpp
@@ -8,19 +8,27 @@ ASOS_Node::~ASOS_Node(){
 
 int ASOS_Node::ProcessMessage(const unsigned char *buff, int buff_size, int ws_opcode){
   int i;
-  printf("[opcode %x] ", ws_opcode);  
-  for(i=0; i<buff_size; i++){
-    if(ws_opcode == 0x1 ){     // text
-      printf("%c", buff[i]);
-    }else if(ws_opcode == 0x2 ){    // BLOB
-      printf("%02x", buff[i]);      
-    }
-    if(i > 70){
-      printf(" ... (size  %d)", buff_size);
-      break;
-    }
-  }
-  printf("\n");  
+  int ret = 1;
   
-  return 1;
+  if(ws_opcode == 0x1 ){     // text
+    ret = -1;                // disconnect.  Because the asos protocol has only binary message. 
+    printf("[websocket opcode %x(=text)] ", ws_opcode);
+    for(i=0; i<buff_size; i++){
+      printf("%c", buff[i]);
+      if(i > 70){ printf(" ... (size  %d)", buff_size); break; }
+    }
+    printf("\n");  
+  }
+
+  if(ws_opcode == 0x2 ){     // BLOB
+    printf("[websocket opcode %x(=blob)] ", ws_opcode);
+    for(i=0; i<buff_size; i++){
+      printf("%02x", buff[i]);      
+      if(i > 70){ printf(" ... (size  %d)", buff_size); break; }
+    }
+    printf("\n");  
+  }
+  
+  return ret;
 }
+
---
+++ src/servers/asos_common/ASOS_message.hpp
@@ -0,0 +1,14 @@
+#ifndef ASOS_MESSAGE_HPP
+#define ASOS_MESSAGE_HPP
+
+class ASOS_message{
+public:
+  ASOS_message();
+  ~ASOS_message();
+
+  
+
+};
+
+#endif
+
